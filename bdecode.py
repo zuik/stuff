804382834c4ba4c7d9ff608fef63084950e54e12
Zui <zuik@users.noreply.github.com>
2016-03-15 09:28:11 -0400

    Update bdecode.py
    - Clean up the code.
    - Get rid of the annoying empty byte that hangs around because of recursive function calls.

--- bdecode.py
+++ bdecode.py
@@ -1,68 +1,42 @@
 def decode_string(text):
-    colon = text.index(b":")
-    length = int(text[:colon].decode())
-    try:
-        ended = text[length+1:] == b'e'
-    except IndexError:
-        ended = False
-    return (#length,
-            text[colon+1:(colon+1+length)],
-            text[colon+1+length:],
-            #ended,
-    )
+    colon = text.index(b":") + 1
+    length = int(text[:colon-1].decode())
+    value, text = text[colon:(colon+length)], text[colon+length:]
+    return (value, text) if len(text) > 0 else value
 
 def decode_int(text):
     length = text.index(b"e")
-    try:
-        ended = text[length+1:] == b'e'
-    except IndexError:
-        ended = False
-    return (int(text[1:length]),
-            text[length+1:],
-            #ended
-           )
+    value, text = text[1:length], text[length+1:]
+    return (value, text) if len(text) > 0 else value
+
 def decode_list(text):
     text = text[1:]
     r = []
-    ended = False
-    n = 100
-    while n>0:
-        #print(text)
-        value, rest = decode(text)
+    while True:
+        value, text = decode(text)
         r.append(value)
-        text = rest
-        #print(value, rest, n)
-        n -= 1
-        if rest[0:1] == b"e":
-            #print(r, rest)
-            return r, rest[1:]
+        if text[0:1] == b"e":
+            return (r, text[1:]) if len(text) > 1 else r
             break
+
 def decode_dict(text):
     text = text[1:]
     r = {}
-    n = 100
-    while n>0:
-        n -= 1
-        key, rest = decode_string(text)
-        text = rest
-        #print(rest, key)
-        value, rest = decode(text)
-        text = rest
+    while True:
+        key, text = decode_string(text)
+        value, text = decode(text)
         r[key] = value
-        if rest[0:1] == b"e":
-            #print(r, rest)
-            return r, rest[1:]
+        if text[0:1] == b"e":
+            return (r, text[1:]) if len(text) > 1 else r
             break
-        #print(value, rest, key)
+
 def decode(text):
-    pointer = text[0:1]
-    #print(pointer)
-    if pointer == b'l':
+    head = text[0:1]
+    if head == b'l':
         return decode_list(text)
-    if pointer == b'i':
-        #print(text[1:])
+    if head == b'i':
         return decode_int(text)
-    if pointer == b'd':
+    if head == b'd':
         return decode_dict(text)
-    if pointer in [b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9']:
+    if head in [b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9']:
         return decode_string(text)
