2bf72eed94f38ad3c9e232803d0f87fdc9460dfa
Duy Nguyen <zuikng@gmail.com>
2017-02-01 01:05:01 -0500

    Nyan-mode for Emacs

--- emacs-init.el
+++ emacs-init.el
@@ -15,7 +15,12 @@
 
 (require 'init-ui)
 
+(require 'nyan-mode)
+
 (provide 'init)
+
+(nyan-mode 1)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
