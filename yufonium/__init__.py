e13744b35c725acec7b64d2ea1ee9e6c8dbe85d6
Duy Nguyen <zuikng@gmail.com>
2017-03-08 23:35:47 -0500

    Extract audio info

--- yufonium/__init__.py
+++ yufonium/__init__.py
@@ -1,6 +1,7 @@
-from yufonium.youtube import get_info
+from yufonium.youtube import get_info, audio_urls
 import json
 
 if __name__ == '__main__':
-    url = 'https://www.youtube.com/watch?v=cLfyjIlu9Uw'
-    print(json.dumps(get_info(url)))
+    # url = 'https://www.youtube.com/watch?v=cLfyjIlu9Uw'
+    # print(json.dumps(get_info(url)))
+    print(audio_urls("test"))
--- yufonium/youtube.py
+++ yufonium/youtube.py
@@ -1,5 +1,25 @@
+import json
 from youtube_dl import YoutubeDL
 
-ydl_options = {
-    
+ydl_opts = {
+    "noplaylist": True  # For now, we will seperate playlist and singles later
 }
+
+
+def get_info(url):
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        return info
+
+
+def audio_urls(url):
+    # info = get_info(url)
+    info = load_test_json('/Users/zui/kode/python/yufonium/yufonium/test_result.json')
+    audio_tracks = [i for i in info['formats'] if i['format_note'] == "DASH audio"]
+    return [{'format': i['format'], 'url': i['url'], 'filesize': i['filesize']} for i in audio_tracks]
+
+
+def load_test_json(filename):
+    with open(filename, 'r') as f:
+        data = f.read()
+        return json.loads(data)
